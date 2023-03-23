be02b02cadd6a6746a7e36103faf3611962ba22a
ZAKARIA BENAICHA <zakaria11benaicha@gmail.com>
2023-03-23 16:22:14 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,14 +4,13 @@
  * Return: 0
  */
 
-
-
 int main(void)
-
 {
-        int i;
-        i = 0;
-        positive_or_negative(i);
+	int i;
 
-        return (0);
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
 }
+
